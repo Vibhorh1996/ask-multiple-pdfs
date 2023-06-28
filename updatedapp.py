08316c83437df595c22442847cd69070964559bd
Vibhorh1996 <vibhorh1996@gmail.com>
2023-06-28 20:20:46 +0530

    Update updatedapp.py

--- updatedapp.py
+++ updatedapp.py
@@ -65,11 +65,9 @@ def handle_userinput(user_question):
 
         for i, message in enumerate(st.session_state.chat_history):
             if i % 2 == 0:
-                st.write(user_template.replace(
-                    "{{MSG}}", message['content']), unsafe_allow_html=True)
+                st.write(user_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
             else:
-                st.write(bot_template.replace(
-                    "{{MSG}}", message['content']), unsafe_allow_html=True)
+                st.write(bot_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
 
 def main():
     load_dotenv()
@@ -85,10 +83,10 @@ def main():
     os.environ['OPENAI_API_KEY'] = key
 
     if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    
+    st.session_state.conversation = []
+
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+    st.session_state.chat_history = []
 
     st.header("Chat with multiple PDFs :books:")
     user_question = st.text_input("Ask a question about your documents:")
