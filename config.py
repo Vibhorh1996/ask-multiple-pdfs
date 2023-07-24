2ec7342803fbdde5023723a37e5fe05166a6dc86
Vibhorh1996 <vibhorh1996@gmail.com>
2023-07-24 16:25:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-deployment_name = "LH-davinci"
+deployment_name = "LH-GPT"
 api_type = "azure"
 api_key = "fb3a0027e2c64a52adae3ba9f4b67583"
 api_base = "https://openai-lh.openai.azure.com/"
