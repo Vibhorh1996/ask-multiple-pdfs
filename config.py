b6f5132b562500bc186f37cf2389bbedbaabf36c
Vibhorh1996 <vibhorh1996@gmail.com>
2023-08-12 20:39:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import openai
 
 deployment_name = "LH-GPT"
 api_type = "azure"
-api_key = "fb3a0027e2c64a52adae3ba9f4b67583"
+api_key = os.environ.get("OPENAI_API_KEY")
 api_base = "https://openai-lh.openai.azure.com/"
 api_version = "2022-12-01"
